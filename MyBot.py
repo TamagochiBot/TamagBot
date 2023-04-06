b277ae4dcdd1a067574eed417aaea4015a0f14a8
romburunduk <rom.burunduk@gmail.com>
2023-04-06 16:08:57 +0300

    update

--- MyBot.py
+++ MyBot.py
@@ -1,33 +1,15 @@
-#!/usr/bin/python
-
-# It echoes any incoming text messages.
-
-# This is a simple echo bot using the decorator mechanism.
 import telebot
-from telebot import types
-import sqlite3, datetime
-API_TOKEN = 'token'
+import sqlite3
+
+from telebot.types import Message
+
+API_TOKEN = '5932887460:AAEqLOtWTfWZrN8j8JJXiS3joAOXWPFYo5I'
 
 bot = telebot.TeleBot(API_TOKEN, skip_pending=True)
 
-conn = sqlite3.connect('UserInfo.db')
-c = conn.cursor()
-conn.commit()
-c.execute('''CREATE TABLE IF NOT EXISTS users (
-        userid INT PRIMARY KEY,
-        fname TEXT,
-        sname TEXT,
-        usrname TEXT,
-        join_data TIMESTAMP);    
-    ''')
-
-conn.commit()
-
-
-
 def search_id_tlg(id_tlgrm):
     conn = sqlite3.connect('UserInfo.db')
-    cursor = conn.execute("SELECT userid FROM users WHERE userid=?", (id_tlgrm,)).fetchone()
+    cursor = conn.execute("SELECT id FROM pet WHERE id=?", (id_tlgrm,)).fetchone()
     if cursor is None:
         conn.close()
         return True
@@ -38,52 +20,58 @@ def search_id_tlg(id_tlgrm):
 
 
 
-bot.send_message(771366061,"Бот работает")
+# bot.send_message(771366061,"Бот работает")
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton('/info')
-    markup.add(button)
-    bot.send_message(message.chat.id, """\
-Hi there, I am TestBot.
-I am here to save information about you.
-Write /info for collecting data.\
-""", reply_markup=markup)
+    if not search_id_tlg(message.from_user.id):
+        bot.send_message(message.chat.id, '''Ты уже зарегестрирован в боте.''')
+    else:
+        bot.send_message(message.chat.id, """Похоже, ты ещё не зарегестрирован, минуту...""")
+        bot.send_message(message.chat.id, """Как будут звать твоего питомца?""")
+        bot.register_next_step_handler(message, registration)
 
 
-@bot.message_handler(commands=['info'])
-def info_message(message):
-    bot.send_message(message.chat.id, 'Getting information...')
-    a = telebot.types.ReplyKeyboardRemove()
+def registration(message: Message):
     with sqlite3.connect('UserInfo.db') as conn:
-        _data = (str(message.from_user.id), str(message.from_user.first_name), str(message.from_user.last_name),
-                 str(message.from_user.username), datetime.datetime.now())
+        cur = conn.cursor()
+        cur.execute('''INSERT INTO pet (id,name) VALUES (?,?);''', (message.from_user.id, str(message.text)))
+        conn.commit()
+    bot.reply_to(message, "Вы успешно зарегестрированы!")
 
-        if search_id_tlg(message.from_user.id):
-            cur = conn.cursor()
-            cur.execute('''INSERT INTO users VALUES (?,?,?,?,?);''', _data)
-            conn.commit()
-            bot.send_message(message.chat.id, 'Collected information',reply_markup=a)
-        else:
-            with sqlite3.connect('UserInfo.db') as connect:
-                cur = connect.cursor()
-                cur.execute('''UPDATE users SET fname = ?, sname = ?, usrname = ? WHERE userid = ?''', [
-                    message.from_user.first_name, message.from_user.last_name, message.from_user.username,
-                    message.from_user.id])
-                connect.commit()
-                bot.send_message(message.chat.id, "Your information has been updated",reply_markup=a)
+#     bot.send_message(message.chat.id, """\
+# Hi there, I am TestBot.
+# I am here to save information about you.
+# Write /info for collecting data.\
+# """)
 
 
+def next_step(message):
+    if message.from_user.text == "Да":
+        bot.send_message(message.chat.id, "Ща")
 
 
-@bot.message_handler(commands=["button1"])
-def bt1(message):
-    markup = types.InlineKeyboardMarkup
-    button = types.InlineKeyboardButton("foo",None,"FOO")
-    markup.add()
-
-
+# @bot.message_handler(commands=['info'])
+# def info_message(message):
+#     bot.send_message(message.chat.id, 'Getting information...')
+#     a = telebot.types.ReplyKeyboardRemove()
+#     with sqlite3.connect('UserInfo.db') as conn:
+#         _data = (str(message.from_user.id), str(message.from_user.first_name), str(message.from_user.last_name),
+#                  str(message.from_user.username), datetime.datetime.now())
+#
+#         if search_id_tlg(message.from_user.id):
+#             cur = conn.cursor()
+#             cur.execute('''INSERT INTO users VALUES (?,?,?,?,?);''', _data)
+#             conn.commit()
+#             bot.send_message(message.chat.id, 'Collected information',reply_markup=a)
+#         else:
+#             with sqlite3.connect('UserInfo.db') as connect:
+#                 cur = connect.cursor()
+#                 cur.execute('''UPDATE users SET fname = ?, sname = ?, usrname = ? WHERE userid = ?''', [
+#                     message.from_user.first_name, message.from_user.last_name, message.from_user.username,
+#                     message.from_user.id])
+#                 connect.commit()
+#                 bot.send_message(message.chat.id, "Your information has been updated",reply_markup=a)
 
 bot.polling(none_stop=True)
